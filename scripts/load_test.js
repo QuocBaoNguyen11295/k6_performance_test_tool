934269188d6eb24c8a7a60ca56057794941527c4
QuocBaoNguyen11295 <nqb11295@gmail.com>
2022-10-10 00:28:41 +0700

    Add load test

--- scripts/load_test.js
+++ scripts/load_test.js
@@ -1,27 +1,24 @@
-import encoding from 'k6/encoding';
 import http from 'k6/http';
 import { check } from 'k6';
+import encoding from 'k6/encoding';
 
 const username = 'baonguyen01121995@gmail.com';
 const password = 'U5vgjiVwxWqvXJ9Pig5H8949';
 
 export const options = {
-  stages: [
-    { duration: '2m', target: 100 }, // below normal load
-    { duration: '5m', target: 100 },
-    { duration: '2m', target: 200 }, // normal load
-    { duration: '5m', target: 200 },
-    { duration: '2m', target: 300 }, // around the breaking point
-    { duration: '5m', target: 300 },
-    { duration: '2m', target: 400 }, // beyond the breaking point
-    { duration: '5m', target: 400 },
-    { duration: '10m', target: 0 }, // scale down. Recovery stage.
-  ],
+    stages: [
+        { duration: '2m', target: 40 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '4m', target: 40 }, // stay at 100 users for 10 minutes
+        { duration: '2m', target: 0 }, // ramp-down to 0 users
+      ],
+      thresholds: {
+        'http_req_duration': ['p(99)<1500'] // 99% of requests must complete below 1.5s
+      },
 }
 export default function () {
   const credentials = `${username}:${password}`;
   const url = `https://baoapitesting.atlassian.net/rest/api/2/issue/`;
-  let data ='{"fields":{"project":{"id":"10000"},"summary":"Bao Stress Test","description": "K6","issuetype":{"name":"Bug"}}}'
+  let data ='{"fields":{"project":{"id":"10000"},"summary":"Bao Load Test","description": "K6","issuetype":{"name":"Task"}}}'
   const encodedCredentials = encoding.b64encode(credentials);
   let res = http.post(url,data,{headers: {Authorization: `Basic ${encodedCredentials}`,"Content-Type": `application/json;charset=UTF-8`}})
   check(res, {
